a6971da2cee1155413c203d86ab6531cd59f99b6
ryooit <ryooit70@gmail.com>
2020-10-30 11:10:19 +0900

    Fix output path of checkpoints

--- examples/translation/run_savvi_multi_output_test.sh
+++ examples/translation/run_savvi_multi_output_test.sh
@@ -40,5 +40,5 @@ CUDA_VISIBLE_DEVICES=0,1 fairseq-train \
 ls output
 
 fairseq-generate data-bin/iwslt14.tokenized.de-en \
-    --path output/checkpoints/checkpoint_best.pt --fp16 \
+    --path output/checkpoint_best.pt --fp16 \
     --batch-size 128 --beam 5 --remove-bpe True --quiet
